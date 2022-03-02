184a1fa31f423362aa5d0c66e64fadec0e43e4f9
Matheus Massula <matheus.massula@ab-inbev.com>
2022-03-02 09:03:38 -0300

    Create routes with nuvigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:proj/route_generator.dart';
+import 'package:nuvigator/next.dart';
+import 'package:proj/models/package_model.dart';
+import 'package:proj/models/producer_model.dart';
+import 'package:proj/screens/favorites_screen.dart';
+import 'package:proj/screens/home_screen.dart';
+import 'package:proj/screens/login_screen.dart';
+import 'package:proj/screens/package_details_screen.dart';
+import 'package:proj/screens/payment_screen.dart';
+import 'package:proj/screens/producer_details_screen.dart';
+import 'package:proj/screens/profile_screen.dart';
+import 'package:proj/screens/singup_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,8 +25,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Montserrat',
       ),
-      initialRoute: 'home',
-      onGenerateRoute: RouteGenerator.generateRoute,
+      home: Nuvigator.routes(
+        initialRoute: 'home',
+        routes: [
+          NuRouteBuilder(path: 'home', builder: (_, __, ___) => HomeScreen()),
+          NuRouteBuilder(path: 'login', builder: (_, __, ___) => LoginScreen()),
+          NuRouteBuilder(path: 'sing-up', builder: (_, __, ___) => SingupScreen()),
+          NuRouteBuilder(path: 'favorites', builder: (_, __, ___) => FavoritesScreen()),
+          NuRouteBuilder(path: 'profile', builder: (_, __, ___) => ProfileScreen()),
+          NuRouteBuilder(path: 'payment', builder: (_, __, ___) => PaymentScreen()),
+          NuRouteBuilder(path: 'producer-details', builder: (_, __, NuRouteSettings args) {
+            final Producer producer = args.rawParameters['producer'];
+            return ProducerDetailsScreen(producer: producer);
+          }),
+          NuRouteBuilder(path: 'package-details', builder: (_, __, NuRouteSettings args) {
+            final Producer producer = args.rawParameters['producer'];
+            final Package package = args.rawParameters['package'];
+            return PackageDetailsScreen(producer: producer, package: package);
+          }),
+        ]
+      ),
     );
   }
 }
