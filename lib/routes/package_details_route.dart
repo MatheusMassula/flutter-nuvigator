1aa78bed67102ad4e0b2276386e734154cb5b7ee
Matheus Massula <matheus.massula@ab-inbev.com>
2022-03-03 14:37:37 -0300

    removing generic objects from being used as route parameters

--- lib/routes/package_details_route.dart
+++ lib/routes/package_details_route.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:nuvigator/next.dart';
+import 'package:proj/models/package_model.dart';
+import 'package:proj/models/producer_model.dart';
 import 'package:proj/screens/package_details_screen.dart';
 
-class PackageDetailsRoute extends NuRoute {
+class PackageDetailsRoute extends NuRoute<NuRouter, PackageDetailsRequestObject, String> {
   @override
-  Widget build(BuildContext context, NuRouteSettings<Object> settings) => PackageDetailsScreen(
-    package: settings.rawParameters['package'],
-    producer: settings.rawParameters['producer'],
+  Widget build(BuildContext context, NuRouteSettings<PackageDetailsRequestObject> settings) => PackageDetailsScreen(
+    package: settings.args.package,
+    producer: settings.args.producer,
   );
 
   @override
@@ -14,5 +16,32 @@ class PackageDetailsRoute extends NuRoute {
 
   @override
   ScreenType get screenType => materialScreenType;
+
+  @override
+  ParamsParser<PackageDetailsRequestObject> get paramsParser => PackageDetailsRequestObject.fromJson;
   
+}
+
+class PackageDetailsRequestObject {
+  final Package package;
+  final Producer producer;
+
+  PackageDetailsRequestObject({
+    @required this.package,
+    @required this.producer
+  });
+
+  // PackageDetailsRequestObject.fromJson(Map<String, dynamic> json) :
+  //   package = json['package'],
+  //   producer = json['producer'];
+
+  static PackageDetailsRequestObject fromJson(Map<String, dynamic> json) => PackageDetailsRequestObject(
+    package: json['package'],
+    producer: json['producer'],
+  );
+
+  Map<String, dynamic> toJson() => {
+    'package': package,
+    'producer': producer,
+  };
 }
